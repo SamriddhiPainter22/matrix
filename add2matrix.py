8710db91cbf448fb6ec8580a1f2032256a29e4b8
Samriddhi Painter <88041669+SamriddhiPainter22@users.noreply.github.com>
2022-02-07 06:35:52 +0530

    first commit

---
+++ add2matrix.py
@@ -0,0 +1,24 @@
+
+
+
+X=[[1,2,3],
+  [4,5,6],
+  [7,8,9]]
+
+Y=[[10,11,12],
+   [13,14,15],
+   [16,17,18]]
+
+Result=[[0,0,0],
+[0,0,0],
+[0,0,0]]
+
+#iterate through rows
+
+for i in range(len(X)):
+    #iterative though columns
+    for j in range(len(X[0])):
+        Result [i][j] = X[i][j] + Y[i][j]
+
+for r in range:
+    print(r)
---
+++ mulusenestedlist.py
@@ -0,0 +1,18 @@
+A=[[5,4,3],
+[2,4,6],
+[4,7,9]]
+
+B=[[3,2,4],
+[4,3,6],
+[2,5,7]]
+
+#using nested list method with zip in Python
+
+multiResult =[[sum(a*b for a,b in zip(Arow,Bcol))
+            for Bcol in zip(*B)]
+                for Arow in A]
+
+#printing multiplication result in th eoutput
+print("The Muktiplication result of matrix A and B is : ")
+for res in multiResult:
+    print(res)
